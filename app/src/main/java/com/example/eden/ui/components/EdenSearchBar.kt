28c09ac08ace5c7e8b6b174c2dadff2b209d7484
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-10-14 00:01:42 +0530

    refactor: add EdenSearchBar component

---
+++ app/src/main/java/com/example/eden/ui/components/EdenSearchBar.kt
@@ -0,0 +1,40 @@
+package com.example.eden.ui.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EdenSearchBar(
+    modifier: Modifier,
+    value: String,
+    label: @Composable () -> Unit = {},
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        leadingIcon = {
+            Icon(
+                modifier = Modifier.size(18.dp),
+                imageVector = Icons.Filled.Search,
+                contentDescription = "searchIcon"
+            )
+        },
+        value = value,
+        onValueChange = onValueChange,
+        textStyle = MaterialTheme.typography.body1,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = MaterialTheme.colors.secondary,
+            focusedLabelColor = MaterialTheme.colors.secondary
+        ),
+        label = label
+    )
+
+}
