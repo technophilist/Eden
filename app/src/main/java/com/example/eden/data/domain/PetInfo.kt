17c54f6a731712d9331081437ee564ce1060e237
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-10-15 12:33:50 +0530

    refactor: add type and breed info in "PetInfo" data class

--- app/src/main/java/com/example/eden/data/domain/PetInfo.kt
+++ app/src/main/java/com/example/eden/data/domain/PetInfo.kt
@@ -5,6 +5,8 @@ import androidx.annotation.DrawableRes
 data class PetInfo(
     val name:String,
     val description:String,
+    val type:String,
+    val breed:String,
     val price:Float,
     @DrawableRes val imageResource:Int
 )
