1ba48faa5ffd20b0e9dba15d1c0fa031fa83e9a6
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-10-15 11:32:00 +0530

    refactor: rename "Pet" data class to "PetInfo"

--- app/src/main/java/com/example/eden/data/domain/PetInfo.kt
+++ app/src/main/java/com/example/eden/data/domain/PetInfo.kt
@@ -2,7 +2,7 @@ package com.example.eden.data.domain
 
 import androidx.annotation.DrawableRes
 
-data class Pet(
+data class PetInfo(
     val name:String,
     val description:String,
     val price:Float,
