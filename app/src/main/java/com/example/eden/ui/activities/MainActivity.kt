1615853959e36f5a19ceae8415ea161f8d234b58
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-10-16 20:52:03 +0530

    refactor: create EdenApp composable

--- app/src/main/java/com/example/eden/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/eden/ui/activities/MainActivity.kt
@@ -3,15 +3,15 @@ package com.example.eden.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
 import androidx.compose.ui.ExperimentalComposeUiApi
-import com.example.eden.ui.screens.LoginScreen
-import com.example.eden.ui.screens.SignUpScreen
-import com.example.eden.ui.screens.WelcomeScreen
+import com.example.eden.ui.screens.EdenApp
 import com.example.eden.ui.theme.EdenAppTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
     @ExperimentalComposeUiApi
     @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             EdenAppTheme {
                 Surface {
-                    LoginScreen()
+                    EdenApp()
                 }
             }
         }
