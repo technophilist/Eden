3e5fbd8516559408f9495cebc34225beecc68616
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-10-21 15:21:51 +0530

    refactor: move NotificationInfo data class to domain package

---
+++ app/src/main/java/com/example/eden/data/domain/NotificationInfo.kt
@@ -0,0 +1,14 @@
+package com.example.eden.data.domain
+
+
+/**
+ * This class holds all information that is related to a notification.
+ */
+data class NotificationInfo(
+    val id: Int,
+    val type: NotificationType,
+    val header: String,
+    val content: String
+) {
+    enum class NotificationType { ORDERS, APPOINTMENTS, NGO }
+}
