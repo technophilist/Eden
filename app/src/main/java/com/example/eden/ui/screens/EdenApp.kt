e334d12f2fb113e476f9c94b2a34edb7e869c5ec
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-10-22 07:47:36 +0530

    refactor: use compose-navigation for BottomNavigationBar

--- app/src/main/java/com/example/eden/ui/screens/EdenApp.kt
+++ app/src/main/java/com/example/eden/ui/screens/EdenApp.kt
@@ -1,117 +1,92 @@
 package com.example.eden.ui.screens
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.example.eden.ui.components.EdenSearchBar
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.eden.data.domain.NotificationInfo
+import com.example.eden.ui.navigation.BottomNavigationRoutes
 import com.example.eden.viewmodels.EdenHomeScreenViewModel
-import com.example.eden.viewmodels.HomeScreenViewModel
 
 @ExperimentalMaterialApi
 @Composable
-fun EdenApp(){
-    val scaffoldState = rememberScaffoldState()
-    var searchText by remember { mutableStateOf("") }
-    Scaffold(
-        scaffoldState = scaffoldState,
-        topBar = {
-            EdenSearchBar(
-                modifier = Modifier
-                    .padding(start = 16.dp, end = 16.dp)
-                    .fillMaxWidth(),
-                label = { Text("Search") },
-                onValueChange = { searchText = it },
-                value = searchText,
+fun EdenApp() {
+    val navController = rememberNavController()
+    val bottomNavigationDestinations = listOf(
+        BottomNavigationRoutes.HomeScreen,
+        BottomNavigationRoutes.NotificationsScreen
+    )
+    val testNotifications = listOf(
+        NotificationInfo(
+            1,
+            NotificationInfo.NotificationType.ORDERS,
+            header = "Hurray! Order Placed !",
+            content = "Lore Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        ),
+        NotificationInfo(
+            1,
+            NotificationInfo.NotificationType.APPOINTMENTS,
+            header = "Appointment Granted !",
+            content = "Lore Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        ),
+        NotificationInfo(
+            1,
+            NotificationInfo.NotificationType.NGO,
+            header = "New NGO event annocement",
+            content = "Lore Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        )
+    )
+    Box(modifier = Modifier.fillMaxSize()) {
+        NavHost(
+            navController = navController,
+            startDestination = BottomNavigationRoutes.HomeScreen.route
+        ) {
+            composable(BottomNavigationRoutes.HomeScreen.route) {
+                HomeScreen(viewmodel = EdenHomeScreenViewModel())
+            }
+            composable(BottomNavigationRoutes.NotificationsScreen.route) {
+                NotificationsScreen(testNotifications)
+            }
+        }
+        EdenBottomNavigation(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            navController = navController,
+            navigationDestinations = bottomNavigationDestinations
+        )
+    }
+}
+
+@Composable
+private fun EdenBottomNavigation(
+    modifier: Modifier,
+    navController: NavController,
+    navigationDestinations: List<BottomNavigationRoutes>
+) {
+    BottomNavigation(modifier = modifier) {
+        val currentBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = currentBackStackEntry?.destination?.route
+        navigationDestinations.forEach {
+            BottomNavigationItem(
+                icon = { Icon(imageVector = it.icon, contentDescription = " ") },
+                selected = currentRoute == it.route,
+                onClick = { if (currentRoute != it.route) navController.navigate(it.route) },
+                label = {
+                    Text(
+                        text = it.label,
+                        style = MaterialTheme.typography.caption,
+                        color = MaterialTheme.colors.onPrimary
+                    )
+                }
             )
-        },
-        bottomBar = { EdenBottomNavigation() }
-    ){
-        Box(modifier = Modifier.padding(it)){
-            HomeScreen(EdenHomeScreenViewModel())
         }
     }
 }
 
-@Composable
-private fun EdenBottomNavigation(modifier: Modifier = Modifier) {
-    BottomNavigation(
-        modifier = modifier,
-        elevation = 16.dp,
-    ) {
-        BottomNavigationItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Filled.Home,
-                    contentDescription = " "
-                )
-            },
-            selected = false,
-            onClick = { /*TODO*/ },
-            label = {
-                Text(
-                    text = "Home",
-                    style = MaterialTheme.typography.caption,
-                    color = MaterialTheme.colors.onPrimary
-                )
-            }
-        )
-        BottomNavigationItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Filled.Notifications,
-                    contentDescription = ""
-                )
-            },
-            selected = false,
-            onClick = { /*TODO*/ },
-            label = {
-                Text(
-                    text = "Notifications", style = MaterialTheme.typography.caption,
-                    color = MaterialTheme.colors.onPrimary
-                )
-            }
-        )
-        BottomNavigationItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Filled.AccountCircle,
-                    contentDescription = " "
-                )
-            },
-            selected = false,
-            onClick = { /*TODO*/ },
-            label = {
-                Text(
-                    text = "Profile", style = MaterialTheme.typography.caption,
-                    color = MaterialTheme.colors.onPrimary
-                )
-            }
-        )
-        BottomNavigationItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Filled.ShoppingCart,
-                    contentDescription = ""
-                )
-            },
-            selected = false,
-            onClick = { /*TODO*/ },
-            label = {
-                Text(
-                    text = "Cart", style = MaterialTheme.typography.caption,
-                    color = MaterialTheme.colors.onPrimary
-                )
-            }
-        )
-    }
-}
