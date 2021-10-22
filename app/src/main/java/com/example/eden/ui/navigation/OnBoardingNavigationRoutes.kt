0164744e9e6ddc005053cd1e47443d51bb097e1f
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-10-22 12:33:12 +0530

    refactor: create onBoardingNavigationRoutes object class

---
+++ app/src/main/java/com/example/eden/ui/navigation/OnBoardingNavigationRoutes.kt
@@ -0,0 +1,7 @@
+package com.example.eden.ui.navigation
+
+object OnBoardingNavigationRoutes {
+    const val welcomeScreenRoute = "route_welcome_screen"
+    const val loginScreenRoute = "route_login_screen"
+    const val signUpScreenRoute = "route_sign_up_screen"
+}
