ede524636b6327d4a94c1d6b08d143827f95f2e2
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-10-22 16:18:02 +0530

    refactor: add home screen route to OnBoardingNavigationRoutes

--- app/src/main/java/com/example/eden/ui/navigation/OnBoardingNavigationRoutes.kt
+++ app/src/main/java/com/example/eden/ui/navigation/OnBoardingNavigationRoutes.kt
@@ -4,4 +4,5 @@ object OnBoardingNavigationRoutes {
     const val welcomeScreenRoute = "route_welcome_screen"
     const val loginScreenRoute = "route_login_screen"
     const val signUpScreenRoute = "route_sign_up_screen"
+    const val homeScreenRoute = "route_home_screen"
 }
