d94e91098f4e0d99d53a1970d1153e7d4dc29e60
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-10-28 17:28:32 +0530

    refactor: add defaultDispatcher param in LoginViewModelFactory

--- app/src/main/java/com/example/eden/utils/ViewModelFactories.kt
+++ app/src/main/java/com/example/eden/utils/ViewModelFactories.kt
@@ -4,10 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.eden.auth.AuthenticationService
 import com.example.eden.viewmodels.EdenLogInViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 
 class LogInViewModelFactory(
+    private val defaultDispatcher: CoroutineDispatcher,
     private val authenticationService: AuthenticationService,
 ) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T = EdenLogInViewModel(authenticationService) as T
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+        EdenLogInViewModel(authenticationService, defaultDispatcher) as T
 }
