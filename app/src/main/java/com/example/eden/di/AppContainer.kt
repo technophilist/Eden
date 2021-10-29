f97fce206b19a1b23e8fed01245fea7968d10370
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-10-29 22:33:15 +0530

    refactor: add LoginViewModelFactory to AppContainer

--- app/src/main/java/com/example/eden/di/AppContainer.kt
+++ app/src/main/java/com/example/eden/di/AppContainer.kt
@@ -2,9 +2,11 @@ package com.example.eden.di
 
 import com.example.eden.auth.AuthenticationService
 import com.example.eden.auth.FirebaseAuthenticationService
+import com.example.eden.utils.LogInViewModelFactory
+import kotlinx.coroutines.Dispatchers
 
 class AppContainer {
-
-    val authenticationService = FirebaseAuthenticationService() as AuthenticationService
-
+    private val defaultDispatcher = Dispatchers.IO
+    private val authenticationService = FirebaseAuthenticationService() as AuthenticationService
+    val loginViewModelFactory = LogInViewModelFactory(authenticationService,defaultDispatcher)
 }
