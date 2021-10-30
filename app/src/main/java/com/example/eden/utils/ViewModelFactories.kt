9ed877d8e94d58141313a49899dbbd0a1270f038
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-10-30 23:03:11 +0530

    refactor: create SignUpViewModelFactory

--- app/src/main/java/com/example/eden/utils/ViewModelFactories.kt
+++ app/src/main/java/com/example/eden/utils/ViewModelFactories.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.eden.auth.AuthenticationService
 import com.example.eden.viewmodels.EdenLogInViewModel
+import com.example.eden.viewmodels.EdenSignUpViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 
 class LogInViewModelFactory(
@@ -13,4 +14,13 @@ class LogInViewModelFactory(
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T =
         EdenLogInViewModel(authenticationService, defaultDispatcher) as T
+}
+
+class SignUpViewModelFactory(
+    private val authenticationService: AuthenticationService,
+    private val defaultDispatcher: CoroutineDispatcher,
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+        EdenSignUpViewModel(authenticationService, defaultDispatcher) as T
 }
