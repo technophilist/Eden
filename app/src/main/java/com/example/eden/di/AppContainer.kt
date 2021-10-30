49384d1a8f8d82d6d61813470587496a521206e2
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-10-31 00:20:08 +0530

    refactor: create instance of SignUpViewModelFactory in AppContainer

--- app/src/main/java/com/example/eden/di/AppContainer.kt
+++ app/src/main/java/com/example/eden/di/AppContainer.kt
@@ -3,10 +3,12 @@ package com.example.eden.di
 import com.example.eden.auth.AuthenticationService
 import com.example.eden.auth.FirebaseAuthenticationService
 import com.example.eden.utils.LogInViewModelFactory
+import com.example.eden.utils.SignUpViewModelFactory
 import kotlinx.coroutines.Dispatchers
 
 class AppContainer {
     private val defaultDispatcher = Dispatchers.IO
     private val authenticationService = FirebaseAuthenticationService() as AuthenticationService
     val loginViewModelFactory = LogInViewModelFactory(authenticationService,defaultDispatcher)
+    val signUpViewModelFactory = SignUpViewModelFactory(authenticationService,defaultDispatcher)
 }
