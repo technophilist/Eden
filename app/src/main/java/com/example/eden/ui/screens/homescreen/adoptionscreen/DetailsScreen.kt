d16862480e623e41684f2db507cfc6f8dd9396e4
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-11-03 12:29:09 +0530

    refactor(DetailsScreen): change package directive to match file directive

--- app/src/main/java/com/example/eden/ui/screens/homescreen/adoptionscreen/DetailsScreen.kt
+++ app/src/main/java/com/example/eden/ui/screens/homescreen/adoptionscreen/DetailsScreen.kt
@@ -1,4 +1,4 @@
-package com.example.eden.ui.screens.homescreen
+package com.example.eden.ui.screens.homescreen.adoptionscreen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
