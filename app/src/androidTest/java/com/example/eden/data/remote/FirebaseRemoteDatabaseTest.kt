f7a6f3593a1b4f13cdbaf5c5d694a6fb446e4b31
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-05 13:59:08 +0530

    test: add FirebaseRemoteDatabaseTest

---
+++ app/src/androidTest/java/com/example/eden/data/remote/FirebaseRemoteDatabaseTest.kt
@@ -0,0 +1,14 @@
+package com.example.eden.data.remote
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class FirebaseRemoteDatabaseTest {
+    private val firebaseRemoteDatabase: RemoteDatabase = FirebaseRemoteDatabase()
+
+    @Test
+    fun petInfoListFetchTest_isSuccessfullyFetched() {
+        // should be able to fetch a list without any exceptions
+        runBlocking { firebaseRemoteDatabase.fetchAllPetsAvailableForAdoption() }
+    }
+}
