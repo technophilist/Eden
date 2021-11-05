c5a81cb7695b07bb7c3f5d01d5fb613a22dbcd7d
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-11-05 16:51:17 +0530

    refactor: add repository to AppContainer

--- app/src/main/java/com/example/eden/di/AppContainer.kt
+++ app/src/main/java/com/example/eden/di/AppContainer.kt
@@ -2,6 +2,9 @@ package com.example.eden.di
 
 import com.example.eden.auth.AuthenticationService
 import com.example.eden.auth.FirebaseAuthenticationService
+import com.example.eden.data.EdenRepository
+import com.example.eden.data.Repository
+import com.example.eden.data.remote.FirebaseRemoteDatabase
 import com.example.eden.utils.LogInViewModelFactory
 import com.example.eden.utils.SignUpViewModelFactory
 import kotlinx.coroutines.Dispatchers
@@ -9,6 +12,8 @@ import kotlinx.coroutines.Dispatchers
 class AppContainer {
     private val defaultDispatcher = Dispatchers.IO
     private val authenticationService = FirebaseAuthenticationService() as AuthenticationService
-    val loginViewModelFactory = LogInViewModelFactory(authenticationService,defaultDispatcher)
-    val signUpViewModelFactory = SignUpViewModelFactory(authenticationService,defaultDispatcher)
+    private val remoteDatabase = FirebaseRemoteDatabase()
+    val loginViewModelFactory = LogInViewModelFactory(authenticationService, defaultDispatcher)
+    val signUpViewModelFactory = SignUpViewModelFactory(authenticationService, defaultDispatcher)
+    val repository = EdenRepository(remoteDatabase) as Repository
 }
