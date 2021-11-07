e26caeb7094b5726a6c68fab6561660f305e35bc
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-07 13:25:04 +0530

    refactor!: replace "fetch pets" method with livedata in Repository

--- app/src/main/java/com/example/eden/data/Repository.kt
+++ app/src/main/java/com/example/eden/data/Repository.kt
@@ -1,20 +1,19 @@
 package com.example.eden.data
 
+import androidx.lifecycle.LiveData
 import com.example.eden.data.domain.EdenUser
 import com.example.eden.data.domain.PetInfo
 import com.example.eden.data.remote.RemoteDatabase
 
 interface Repository {
-    suspend fun fetchAllPetsAvailableForAdoption(): List<PetInfo>
+    val petsAvailableForAdoption: LiveData<List<PetInfo>>
     suspend fun sendRequestForAdoption(user: EdenUser, petInfo: PetInfo)
 }
 
 class EdenRepository(
     private val remoteDatabase: RemoteDatabase
 ) : Repository {
-    override suspend fun fetchAllPetsAvailableForAdoption(): List<PetInfo> =
-        remoteDatabase.fetchAllPetsAvailableForAdoption()
-
+    override val petsAvailableForAdoption:LiveData<List<PetInfo>> = remoteDatabase.petsAvailableForAdoption
     override suspend fun sendRequestForAdoption(user: EdenUser, petInfo: PetInfo) {
         remoteDatabase.sendRequestForAdoption(user.id, petInfo.id)
     }
