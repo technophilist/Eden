e26e62053d84c90aea8a125176e02790f76d3425
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-07 13:59:06 +0530

    refactor!: make featured/recommended list as Livedata

--- app/src/main/java/com/example/eden/viewmodels/AdoptionScreenViewModel.kt
+++ app/src/main/java/com/example/eden/viewmodels/AdoptionScreenViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.eden.viewmodels
 
+import androidx.compose.runtime.State
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.eden.data.Repository
 import com.example.eden.data.domain.PetInfo
@@ -7,8 +10,8 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 interface AdoptionScreenViewModel {
-    val featuredList: List<PetInfo> // change to live data
-    val recommendedList: List<PetInfo> // change to live data
+    val featuredList: LiveData<List<PetInfo>> 
+    val recommendedList: LiveData<List<PetInfo>>
     fun addPetToFavourites(petInfo: PetInfo)
     fun applyFilter(filterOptions: FilterOptions)
     enum class FilterOptions { ALL, DOGS, CATS }
@@ -18,16 +21,11 @@ class EdenAdoptionScreenViewModel(
     private val repository: Repository,
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : ViewModel(), AdoptionScreenViewModel {
-    override val featuredList: List<PetInfo>
-        get() = emptyList()
-
-    override val recommendedList: List<PetInfo>
-        get() = emptyList()
-
+    override val featuredList: LiveData<List<PetInfo>> = repository.petsAvailableForAdoption
+    override val recommendedList: LiveData<List<PetInfo>> = repository.petsAvailableForAdoption // TODO
     override fun addPetToFavourites(petInfo: PetInfo) {
         TODO("Not yet implemented")
     }
-
     override fun applyFilter(filterOptions: AdoptionScreenViewModel.FilterOptions) {
         TODO("Not yet implemented")
     }
