8ea92213fbedbb62a6884a8f4d0cdaf7c9388724
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-07 12:48:48 +0530

    refactor: add AdoptionScreenViewModelFactory to AppContainer

--- app/src/main/java/com/example/eden/di/AppContainer.kt
+++ app/src/main/java/com/example/eden/di/AppContainer.kt
@@ -5,6 +5,7 @@ import com.example.eden.auth.FirebaseAuthenticationService
 import com.example.eden.data.EdenRepository
 import com.example.eden.data.Repository
 import com.example.eden.data.remote.FirebaseRemoteDatabase
+import com.example.eden.utils.AdoptionScreenViewModelFactory
 import com.example.eden.utils.LogInViewModelFactory
 import com.example.eden.utils.SignUpViewModelFactory
 import kotlinx.coroutines.Dispatchers
@@ -12,8 +13,11 @@ import kotlinx.coroutines.Dispatchers
 class AppContainer {
     private val defaultDispatcher = Dispatchers.IO
     private val authenticationService = FirebaseAuthenticationService() as AuthenticationService
+    //repository
     private val remoteDatabase = FirebaseRemoteDatabase()
+    private val repository = EdenRepository(remoteDatabase) as Repository
+    // viewmodel factories
     val loginViewModelFactory = LogInViewModelFactory(authenticationService, defaultDispatcher)
     val signUpViewModelFactory = SignUpViewModelFactory(authenticationService, defaultDispatcher)
-    val repository = EdenRepository(remoteDatabase) as Repository
+    val adoptionScreenViewModelFactory = AdoptionScreenViewModelFactory(repository,defaultDispatcher)
 }
