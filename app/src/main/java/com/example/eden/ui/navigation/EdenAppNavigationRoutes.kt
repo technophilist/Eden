bda826be4f7dd91a40ce4ac8f4c9ef9ab1a74293
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-11-11 15:57:34 +0530

    refactor(LoginScreen): remove nav controller and move logic to callback

---
+++ app/src/main/java/com/example/eden/ui/navigation/EdenAppNavigationRoutes.kt
@@ -0,0 +1,7 @@
+package com.example.eden.ui.navigation
+
+object EdenAppNavigationRoutes {
+    const val onBoardingRoute = "com.example.eden.ui.navigation.onBoardingRoute"
+    const val homeScreenRoute = "com.example.eden.ui.navigation.homeScreenRoute"
+
+}
