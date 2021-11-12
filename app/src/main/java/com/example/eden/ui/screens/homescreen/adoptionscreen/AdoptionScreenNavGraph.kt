215ceb67f9792458c169882f5b7a63f8cfb7a8e0
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-12 12:24:07 +0530

    refactor: pass seletedPet to DetailsScreen

--- app/src/main/java/com/example/eden/ui/screens/homescreen/adoptionscreen/AdoptionScreenNavGraph.kt
+++ app/src/main/java/com/example/eden/ui/screens/homescreen/adoptionscreen/AdoptionScreenNavGraph.kt
@@ -1,18 +1,14 @@
 package com.example.eden.ui.screens.homescreen.adoptionscreen
 
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
+import com.example.eden.data.domain.PetInfo
 import com.example.eden.ui.navigation.AdoptionScreenNavigationRoutes
 import com.example.eden.utils.AdoptionScreenViewModelFactory
-import com.example.eden.viewmodels.EdenAdoptionScreenViewModel
 
 
 @ExperimentalMaterialApi
@@ -25,13 +21,15 @@ fun NavGraphBuilder.adoptionScreenGraph(
         composable(route = AdoptionScreenNavigationRoutes.homeScreenRoute) {
             AdoptionScreen(
                 viewmodel = viewModel(it, factory = adoptionScreenViewModelFactory),
-                onItemClicked = {
+                onItemClicked = { selectedPetInfo ->
+                    navController.currentBackStackEntry?.savedStateHandle?.set("a", selectedPetInfo)
                     navController.navigate(AdoptionScreenNavigationRoutes.detailsScreenRoute)
                 }
             )
         }
         composable(route = AdoptionScreenNavigationRoutes.detailsScreenRoute) {
-            DetailsScreen()
+            navController.previousBackStackEntry?.savedStateHandle?.get<PetInfo>("a")
+                ?.let { selectedPet -> DetailsScreen(selectedPet) }
         }
     }
 }
