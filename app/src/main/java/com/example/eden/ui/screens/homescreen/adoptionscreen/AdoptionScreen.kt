ddd64fc1557e26e5ecd26fa98aa635e15dfcffda
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-13 12:59:07 +0530

    feat(AdoptionScreen): display snackbar when like button is clicked

--- app/src/main/java/com/example/eden/ui/screens/homescreen/adoptionscreen/AdoptionScreen.kt
+++ app/src/main/java/com/example/eden/ui/screens/homescreen/adoptionscreen/AdoptionScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,6 +23,7 @@ import com.example.eden.data.domain.PetInfo
 import com.example.eden.ui.components.FilterChip
 import com.example.eden.ui.components.PetCarouselCard
 import com.example.eden.viewmodels.AdoptionScreenViewModel
+import kotlinx.coroutines.launch
 
 @ExperimentalMaterialApi
 @Composable
@@ -31,74 +31,106 @@ fun AdoptionScreen(
     viewmodel: AdoptionScreenViewModel,
     onItemClicked: (PetInfo) -> Unit
 ) {
-    val scrollState = rememberScrollState()
     val featuredList by viewmodel.featuredList
     val recommendedList by viewmodel.recommendedList
     val currentlyAppliedFilter by viewmodel.currentlyAppliedFilter
-    Column(modifier = Modifier.fillMaxSize()) {
-        // chip Group
-        Row(
-            modifier = Modifier
-                .horizontalScroll(scrollState)
-                .padding(start = 8.dp, top = 16.dp)
-                .fillMaxWidth()
-        ) {
-            AdoptionScreenViewModel.FilterOptions.values().forEach { selectedFilter ->
-                FilterChip(
-                    onClick = { viewmodel.applyFilter(selectedFilter) },
-                    isSelected = currentlyAppliedFilter == selectedFilter,
-                    content = {
-                        Text(
-                            text = selectedFilter.name
-                                .lowercase()
-                                .replaceFirstChar { it.uppercase() })
-                    }
-                )
-                Spacer(modifier = Modifier.width(8.dp))
+    val coroutineScope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+    val scrollState = rememberScrollState()
+
+    Box {
+        Column(modifier = Modifier.fillMaxSize()) {
+            // chip Group
+            Row(
+                modifier = Modifier
+                    .horizontalScroll(scrollState)
+                    .padding(start = 8.dp, top = 16.dp)
+                    .fillMaxWidth()
+            ) {
+                AdoptionScreenViewModel.FilterOptions.values().forEach { selectedFilter ->
+                    FilterChip(
+                        onClick = { viewmodel.applyFilter(selectedFilter) },
+                        isSelected = currentlyAppliedFilter == selectedFilter,
+                        content = {
+                            Text(
+                                text = selectedFilter.name
+                                    .lowercase()
+                                    .replaceFirstChar { it.uppercase() })
+                        }
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+            }
+            // featured pets - header
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Featured Pets",
+                style = MaterialTheme.typography.h1,
+                color = MaterialTheme.colors.onPrimary
+            )
+            // featured pets list
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(start = 8.dp, end = 8.dp)
+            ) {
+                items(featuredList) { item ->
+                    PetCarouselCard(
+                        modifier = Modifier.size(200.dp),
+                        petInfo = item,
+                        onClick = { onItemClicked(item) }
+                    )
+                    Spacer(modifier = Modifier.size(10.dp))
+                }
+            }
+            // recommended pets - header
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Recommended Pets",
+                style = MaterialTheme.typography.h1,
+                color = MaterialTheme.colors.onPrimary
+            )
+            // recommended pets - list
+            LazyColumn(modifier = Modifier.padding(start = 8.dp, end = 8.dp)) {
+                items(recommendedList) { petInfo ->
+                    var isPetFavourited by remember { mutableStateOf(false) }
+                    PetInfoCard(
+                        petInfo = petInfo,
+                        isLiked = isPetFavourited,
+                        onLikeButtonClicked = {
+                            isPetFavourited = !isPetFavourited
+                            coroutineScope.launch {
+                                onLikeButtonClicked(
+                                    snackBarHostState,
+                                    viewmodel,
+                                    petInfo,
+                                    isPetFavourited
+                                )
+                            }
+                        },
+                        onClick = { onItemClicked(petInfo) }
+                    )
+                    Spacer(modifier = Modifier.padding(4.dp))
+                }
             }
         }
-        // featured pets - header
-        Text(
-            modifier = Modifier.padding(8.dp),
-            text = "Featured Pets",
-            style = MaterialTheme.typography.h1,
-            color = MaterialTheme.colors.onPrimary
+        SnackbarHost(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            hostState = snackBarHostState
         )
-        // featured pets list
-        LazyRow(
-            modifier = Modifier.fillMaxWidth(),
-            contentPadding = PaddingValues(start = 8.dp, end = 8.dp)
-        ) {
-            items(featuredList ?: emptyList()) { item ->
-                PetCarouselCard(
-                    modifier = Modifier.size(200.dp),
-                    petInfo = item,
-                    onClick = { onItemClicked(item) }
-                )
-                Spacer(modifier = Modifier.size(10.dp))
-            }
-        }
-        // recommended pets - header
-        Text(
-            modifier = Modifier.padding(8.dp),
-            text = "Recommended Pets",
-            style = MaterialTheme.typography.h1,
-            color = MaterialTheme.colors.onPrimary
-        )
-        // recommended pets - list
-        LazyColumn(modifier = Modifier.padding(start = 8.dp, end = 8.dp)) {
-            items(recommendedList ?: emptyList()) { petInfo ->
-                var isPetFavourited by remember { mutableStateOf(false) }
-                PetInfoCard(
-                    petInfo = petInfo,
-                    isLiked = isPetFavourited,
-                    onLikeButtonClicked = { isPetFavourited = !isPetFavourited },
-                    onClick = { onItemClicked(petInfo) }
-                )
-                Spacer(modifier = Modifier.padding(4.dp))
-            }
-        }
     }
+
+}
+
+private suspend fun onLikeButtonClicked(
+    hostState: SnackbarHostState,
+    viewmodel: AdoptionScreenViewModel,
+    petInfo: PetInfo,
+    isPetFavourited: Boolean
+) {
+    hostState.currentSnackbarData?.dismiss()
+    hostState.showSnackbar(if (isPetFavourited) "Added pet to favourites" else "Removed pet from favourites")
+    if (isPetFavourited) viewmodel.addPetToFavourites(petInfo)
+//    else viewmodel.removePetFromFavourites(petInfo) TODO
 }
 
 
