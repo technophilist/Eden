37a2968ccd3c6666390fe437d86b9c37dea71986
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-19 13:02:18 +0530

    refactor!: add "Notifications" prefix to NotificationScreenViewModel

--- app/src/main/java/com/example/eden/viewmodels/NotificationsScreenViewModel.kt
+++ app/src/main/java/com/example/eden/viewmodels/NotificationsScreenViewModel.kt
@@ -5,13 +5,13 @@ import androidx.lifecycle.ViewModel
 import com.example.eden.data.Repository
 import com.example.eden.data.domain.NotificationInfo
 
-interface NotificationScreenViewModel {
+interface NotificationsScreenViewModel {
     val notificationList: LiveData<List<NotificationInfo>>
 }
 
-class EdenNotificationScreenViewmodel(
+class EdenNotificationsScreenViewmodel(
     private val repository: Repository
-) : ViewModel(), NotificationScreenViewModel {
+) : ViewModel(), NotificationsScreenViewModel {
     override val notificationList: LiveData<List<NotificationInfo>>
         get() = TODO("Not yet implemented")
 
