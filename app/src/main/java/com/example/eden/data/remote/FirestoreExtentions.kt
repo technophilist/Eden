b59e56b908a619586bde5ea7754efb22fbe91f8d
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-19 12:37:31 +0530

    refactor: add DocumentSnapshot::toNotificationInfo method

--- app/src/main/java/com/example/eden/data/remote/FirestoreExtentions.kt
+++ app/src/main/java/com/example/eden/data/remote/FirestoreExtentions.kt
@@ -1,5 +1,6 @@
 package com.example.eden.data.remote
 
+import com.example.eden.data.domain.NotificationInfo
 import com.example.eden.data.domain.PetInfo
 import com.google.firebase.firestore.DocumentSnapshot
 
@@ -16,3 +17,19 @@ fun DocumentSnapshot.toPetInfo() = PetInfo(
     color = get("color").toString(),
     weight = get("weight").toString()
 )
+
+fun DocumentSnapshot.toNotificationInfo() = NotificationInfo(
+    id = get("id").toString(),
+    type = getNotificationTypeForString(get("type").toString()),
+    header = get("header").toString(),
+    content = get("content").toString(),
+    urlString = get("urlString").toString()
+)
+
+private fun getNotificationTypeForString(string: String): NotificationInfo.NotificationType =
+    when (string.lowercase()) {
+        "orders" -> NotificationInfo.NotificationType.ORDERS
+        "appointments" -> NotificationInfo.NotificationType.APPOINTMENTS
+        "ngo" -> NotificationInfo.NotificationType.NGO
+        else -> throw IllegalArgumentException("Unexpected value for field name 'type' found in document")
+    }
