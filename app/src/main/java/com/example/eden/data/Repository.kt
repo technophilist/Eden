84b4e864adc9f3b0c1e1ac6624261657f33723f5
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-19 12:43:38 +0530

    refactor!: add Repository#listenForNotifications method

--- app/src/main/java/com/example/eden/data/Repository.kt
+++ app/src/main/java/com/example/eden/data/Repository.kt
@@ -2,18 +2,25 @@ package com.example.eden.data
 
 import androidx.lifecycle.LiveData
 import com.example.eden.data.domain.EdenUser
+import com.example.eden.data.domain.NotificationInfo
 import com.example.eden.data.domain.PetInfo
 import com.example.eden.data.remote.RemoteDatabase
 
 interface Repository {
     val petsAvailableForAdoption: LiveData<List<PetInfo>>
+    fun listenForNotifications(currentUser: EdenUser): LiveData<List<NotificationInfo>>
     suspend fun sendRequestForAdoption(user: EdenUser, petInfo: PetInfo)
 }
 
 class EdenRepository(
     private val remoteDatabase: RemoteDatabase
 ) : Repository {
-    override val petsAvailableForAdoption:LiveData<List<PetInfo>> = remoteDatabase.petsAvailableForAdoption
+    override val petsAvailableForAdoption: LiveData<List<PetInfo>> =
+        remoteDatabase.petsAvailableForAdoption
+
+    override fun listenForNotifications(currentUser: EdenUser) =
+        remoteDatabase.listenForNotifications(currentUser)
+
     override suspend fun sendRequestForAdoption(user: EdenUser, petInfo: PetInfo) {
         remoteDatabase.sendRequestForAdoption(user.id, petInfo.id)
     }
