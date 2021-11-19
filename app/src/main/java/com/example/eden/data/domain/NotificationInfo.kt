208d1aad5ae94a7694cfdf885611064cb74a8380
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-19 12:01:19 +0530

    refactor: make id as a String type in NotificationInfo class

--- app/src/main/java/com/example/eden/data/domain/NotificationInfo.kt
+++ app/src/main/java/com/example/eden/data/domain/NotificationInfo.kt
@@ -5,7 +5,7 @@ package com.example.eden.data.domain
  * This class holds all information that is related to a notification.
  */
 data class NotificationInfo(
-    val id: Int,
+    val id: String,
     val type: NotificationType,
     val header: String,
     val content: String,
