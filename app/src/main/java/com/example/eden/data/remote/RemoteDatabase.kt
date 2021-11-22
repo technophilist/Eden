52bdc66b148275fce61f62251bd44bbf92ad9fdc
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-11-22 12:15:33 +0530

    refactor!: add RemoteDatabase#saveIncidentReport

--- app/src/main/java/com/example/eden/data/remote/RemoteDatabase.kt
+++ app/src/main/java/com/example/eden/data/remote/RemoteDatabase.kt
@@ -2,6 +2,7 @@ package com.example.eden.data.remote
 
 import androidx.lifecycle.LiveData
 import com.example.eden.data.domain.EdenUser
+import com.example.eden.data.domain.IncidentReportInfo
 import com.example.eden.data.domain.NotificationInfo
 import com.example.eden.data.domain.PetInfo
 
@@ -9,4 +10,5 @@ interface RemoteDatabase {
     val petsAvailableForAdoption:LiveData<List<PetInfo>>
     fun listenForNotifications(currentUser:EdenUser):LiveData<List<NotificationInfo>>
     suspend fun sendRequestForAdoption(userId: String, petId: String)
+    suspend fun saveIncidentReport(reportInfo: IncidentReportInfo)
 }
