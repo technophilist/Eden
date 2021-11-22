7a4763940f1ecdd9f9c55359dd446fa3622836c7
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-22 12:42:58 +0530

    refactor!: change image field type to Bitmap in IncidentReportInfo

--- app/src/main/java/com/example/eden/data/domain/IncidentReportInfo.kt
+++ app/src/main/java/com/example/eden/data/domain/IncidentReportInfo.kt
@@ -1,18 +1,18 @@
 package com.example.eden.data.domain
 
-import androidx.compose.ui.graphics.ImageBitmap
+import android.graphics.Bitmap
 
 /**
  * A class that contains all the information related to
  * an incident.
  */
 data class IncidentReportInfo(
-    val name:String,
-    val email:String,
-    val address:String,
-    val city:String,
-    val state:String,
-    val typeOfIncident:String,
-    val description:String,
-    val image:ImageBitmap
+    val name: String,
+    val email: String,
+    val address: String,
+    val city: String,
+    val state: String,
+    val typeOfIncident: String,
+    val description: String,
+    val image: Bitmap
 )
