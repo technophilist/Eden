54a97ea56aac750179fe87dabbff4cd8483786d7
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-22 12:12:14 +0530

    refactor: add ReporScreenViewModel to AppContainer

--- app/src/main/java/com/example/eden/di/AppContainer.kt
+++ app/src/main/java/com/example/eden/di/AppContainer.kt
@@ -5,10 +5,7 @@ import com.example.eden.auth.FirebaseAuthenticationService
 import com.example.eden.data.EdenRepository
 import com.example.eden.data.Repository
 import com.example.eden.data.remote.FirebaseRemoteDatabase
-import com.example.eden.utils.AdoptionScreenViewModelFactory
-import com.example.eden.utils.LogInViewModelFactory
-import com.example.eden.utils.NotificationScreenViewModelFactory
-import com.example.eden.utils.SignUpViewModelFactory
+import com.example.eden.utils.*
 import kotlinx.coroutines.Dispatchers
 
 class AppContainer {
@@ -19,9 +16,10 @@ class AppContainer {
     private val remoteDatabase = FirebaseRemoteDatabase()
     private val repository = EdenRepository(remoteDatabase) as Repository
 
-    // viewmodel factories
+    // viewmodel factories TODO Fix - All viewModel factories will be kept in memory until the application is killed.
     val loginViewModelFactory = LogInViewModelFactory(authenticationService, defaultDispatcher)
     val signUpViewModelFactory = SignUpViewModelFactory(authenticationService, defaultDispatcher)
     val adoptionScreenViewModelFactory = AdoptionScreenViewModelFactory(repository, authenticationService, defaultDispatcher)
     val notificationScreenViewModelFactory = NotificationScreenViewModelFactory(repository,authenticationService)
+    val reportScreenViewModelFactory = ReportsScreenViewModelFactory(repository,authenticationService,defaultDispatcher)
 }
