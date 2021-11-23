fbf554bf963b98c50a64201748992c7ac9ccb6ef
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2021-11-23 12:16:04 +0530

    Revert "refactor: implement FirebaseRemoteDatabase#saveIncidentReport"
    This reverts commit 49f08d9e4dd8b8b23f247837b893ecca0c0e4114.

--- app/src/main/java/com/example/eden/data/remote/FirebaseRemoteDatabase.kt
+++ app/src/main/java/com/example/eden/data/remote/FirebaseRemoteDatabase.kt
@@ -1,24 +1,18 @@
 package com.example.eden.data.remote
 
-import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.eden.data.domain.EdenUser
-import com.example.eden.data.domain.IncidentReportInfo
 import com.example.eden.data.domain.NotificationInfo
 import com.example.eden.data.domain.PetInfo
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
 import kotlinx.coroutines.tasks.await
 import timber.log.Timber
-import java.io.ByteArrayOutputStream
-import java.util.*
 
 class FirebaseRemoteDatabase : RemoteDatabase {
     private val fireStore = Firebase.firestore
-    private val firebaseStorage = Firebase.storage
     private val _petsAvailableForAdoption = MutableLiveData(emptyList<PetInfo>())
     override val petsAvailableForAdoption = _petsAvailableForAdoption as LiveData<List<PetInfo>>
 
@@ -35,12 +29,6 @@ class FirebaseRemoteDatabase : RemoteDatabase {
                 }
             }
     }
-
-    private fun Bitmap.toByteArray() = ByteArrayOutputStream().use {
-        compress(Bitmap.CompressFormat.JPEG, 100, it)
-        it.toByteArray()
-    }
-
 
     /**
      * Used when a user with [userId] wants to send a request for
@@ -84,52 +72,12 @@ class FirebaseRemoteDatabase : RemoteDatabase {
         return liveData
     }
 
-    override suspend fun saveIncidentReport(reportInfo: IncidentReportInfo) {
-        runCatching {
-            val imageByteArray = reportInfo.image.toByteArray()
-            // generate unique identifier for image name
-            val uuidString = UUID.nameUUIDFromBytes(imageByteArray).toString()
-            // upload image to cloud storage
-            val uploadTaskSnapShot = firebaseStorage.reference
-                .child("$INCIDENT_REPORT_IMAGE_STORAGE_LOCATION_PATH/$uuidString")
-                .putBytes(imageByteArray)
-                .await()
-            // get the download URL of uploaded image
-            val downloadUrlForImage = uploadTaskSnapShot.metadata?.reference?.downloadUrl?.await()
-            // save to fireStore
-            fireStore.collection(INCIDENT_REPORT_COLLECTION_PATH)
-                .add(reportInfo.toMap(downloadUrlForImage.toString()))
-                .await()
-        }.getOrElse { Timber.w("${it.message}: Failed to save incident request.") }
-    }
-
-    /**
-     * A map function is required because the [IncidentReportInfo.image] is
-     * of type [Bitmap], which is not possible to store in cloud firestore.
-     * Therefore we instead associate the [downloadUrlString] of the related
-     * image to the field name 'imageURL'.
-     */
-    private fun IncidentReportInfo.toMap(downloadUrlString: String) = mapOf(
-        "name" to name,
-        "email" to email,
-        "address" to address,
-        "city" to city,
-        "state" to state,
-        "typeOfIncident" to typeOfIncident,
-        "description" to description,
-        "imageURL" to downloadUrlString
-    )
 
     companion object {
         private const val PETS_AVAILABLE_FOR_ADOPTION_COLLECTION_PATH =
             "mobile/pet-adoption/availableForAdoption"
         private const val PETS_REQUESTED_FOR_ADOPTION_COLLECTION_PATH =
             "mobile/pet-adoption/requestedForAdoption"
-        private const val RECENTLY_SENT_NOTIFICATIONS_COLLECTION_PATH =
-            "mobile/notifications/recentlySent"
-        private const val INCIDENT_REPORT_IMAGE_STORAGE_LOCATION_PATH =
-            "EDEN/mobile-client/incident-report-images/"
-        private const val INCIDENT_REPORT_COLLECTION_PATH =
-            "mobile/incident-reports/active-reports"
+        private const val RECENTLY_SENT_NOTIFICATIONS_COLLECTION_PATH = "mobile/notifications/recentlySent"
     }
 }
