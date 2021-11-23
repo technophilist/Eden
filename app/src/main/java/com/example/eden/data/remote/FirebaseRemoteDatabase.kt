e325f8c91a3d48de922ac397c0d98ce04e696029
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-11-23 12:32:50 +0530

    refactor: implement saveIncidentReport() in FirebaseRemoteDatabase

--- app/src/main/java/com/example/eden/data/remote/FirebaseRemoteDatabase.kt
+++ app/src/main/java/com/example/eden/data/remote/FirebaseRemoteDatabase.kt
@@ -1,18 +1,21 @@
 package com.example.eden.data.remote
 
+import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.eden.data.domain.EdenUser
-import com.example.eden.data.domain.NotificationInfo
-import com.example.eden.data.domain.PetInfo
+import com.example.eden.data.domain.*
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import kotlinx.coroutines.tasks.await
 import timber.log.Timber
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 class FirebaseRemoteDatabase : RemoteDatabase {
     private val fireStore = Firebase.firestore
+    private val firebaseStorage = Firebase.storage
     private val _petsAvailableForAdoption = MutableLiveData(emptyList<PetInfo>())
     override val petsAvailableForAdoption = _petsAvailableForAdoption as LiveData<List<PetInfo>>
 
@@ -60,6 +63,35 @@ class FirebaseRemoteDatabase : RemoteDatabase {
         }.getOrElse { Timber.w("${it.cause}: Failed to send adoption request.") }
     }
 
+    override suspend fun saveIncidentReport(
+        userId: String,
+        email: String,
+        reportInfo: IncidentReportInfo
+    ) {
+        runCatching {
+            val imageByteArray = reportInfo.image.toByteArray()
+            // generate unique identifier for image name
+            val uuidString = UUID.nameUUIDFromBytes(imageByteArray).toString()
+            // upload image to cloud storage
+            val uploadTaskSnapShot = firebaseStorage.reference
+                .child("$INCIDENT_REPORT_IMAGE_STORAGE_LOCATION_PATH/$uuidString")
+                .putBytes(imageByteArray)
+                .await()
+            // get the download URL of uploaded image
+            val downloadUrlForImage = uploadTaskSnapShot.metadata?.reference?.downloadUrl?.await()
+            // save to fireStore
+            fireStore.collection(INCIDENT_REPORT_COLLECTION_PATH)
+                .add(
+                    reportInfo.toIncidentReportInfoDTO(
+                        userID = userId,
+                        email = email,
+                        imageURLString = downloadUrlForImage.toString()
+                    )
+                )
+                .await()
+        }.getOrElse { Timber.w("${it.message}: Failed to save incident request.") }
+    }
+
     override fun listenForNotifications(currentUser: EdenUser): LiveData<List<NotificationInfo>> {
         val liveData = MutableLiveData(emptyList<NotificationInfo>())
         fireStore.collection(RECENTLY_SENT_NOTIFICATIONS_COLLECTION_PATH)
@@ -73,11 +105,21 @@ class FirebaseRemoteDatabase : RemoteDatabase {
     }
 
 
+    private fun Bitmap.toByteArray() = ByteArrayOutputStream().use {
+        compress(Bitmap.CompressFormat.JPEG, 100, it)
+        it.toByteArray()
+    }
+
     companion object {
         private const val PETS_AVAILABLE_FOR_ADOPTION_COLLECTION_PATH =
             "mobile/pet-adoption/availableForAdoption"
         private const val PETS_REQUESTED_FOR_ADOPTION_COLLECTION_PATH =
             "mobile/pet-adoption/requestedForAdoption"
-        private const val RECENTLY_SENT_NOTIFICATIONS_COLLECTION_PATH = "mobile/notifications/recentlySent"
+        private const val RECENTLY_SENT_NOTIFICATIONS_COLLECTION_PATH =
+            "mobile/notifications/recentlySent"
+        private const val INCIDENT_REPORT_IMAGE_STORAGE_LOCATION_PATH =
+            "EDEN/mobile-client/incident-report-images/"
+        private const val INCIDENT_REPORT_COLLECTION_PATH =
+            "mobile/incident-reports/active-reports"
     }
 }
