77dd7dcf06a259430961506f1b2704d724c543cd
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-11-23 12:44:05 +0530

    refactor!: implement EdenScreenViewModel#sendReport

--- app/src/main/java/com/example/eden/viewmodels/ReportScreenViewModel.kt
+++ app/src/main/java/com/example/eden/viewmodels/ReportScreenViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.eden.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.eden.auth.AuthenticationService
 import com.example.eden.data.Repository
 import com.example.eden.data.domain.IncidentReportInfo
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 interface ReportScreenViewModel {
     fun sendReport(incidentReport: IncidentReportInfo)
@@ -14,9 +16,13 @@ interface ReportScreenViewModel {
 class EdenReportScreenViewModel(
     private val repository: Repository,
     private val authenticationService: AuthenticationService,
-    private val defaultDispatcher:CoroutineDispatcher = Dispatchers.IO
-):ViewModel(),ReportScreenViewModel{
+    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : ViewModel(), ReportScreenViewModel {
     override fun sendReport(incidentReport: IncidentReportInfo) {
-
+        authenticationService.currentUser?.let { currentUser ->
+            viewModelScope.launch(defaultDispatcher) {
+                repository.sendIncidentReport(currentUser, incidentReport)
+            }
+        }
     }
 }
