17708b9db42d2ba952c32cb1391200319510ef73
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-11-23 12:34:15 +0530

    refactor!: remove redundant fields in IncidentReportInfo and create DTO

--- app/src/main/java/com/example/eden/data/domain/IncidentReportInfo.kt
+++ app/src/main/java/com/example/eden/data/domain/IncidentReportInfo.kt
@@ -1,18 +1,37 @@
 package com.example.eden.data.domain
 
 import android.graphics.Bitmap
+import com.example.eden.data.dto.IncidentReportInfoDTO
 
 /**
  * A class that contains all the information related to
  * an incident.
  */
 data class IncidentReportInfo(
-    val name: String,
-    val email: String,
     val address: String,
     val city: String,
     val state: String,
     val typeOfIncident: String,
     val description: String,
     val image: Bitmap
+)
+
+/**
+ * Used to convert an instance of [IncidentReportInfo]domain object
+ * to an instance of [IncidentReportInfoDTO] with the specified
+ * [userID],[email] and [imageURLString]
+ */
+fun IncidentReportInfo.toIncidentReportInfoDTO(
+    userID: String,
+    email: String,
+    imageURLString: String
+) = IncidentReportInfoDTO(
+    userID = userID,
+    email = email,
+    address = address,
+    city = city,
+    state = state,
+    typeOfIncident = typeOfIncident,
+    description = description,
+    imageURL = imageURLString
 )
