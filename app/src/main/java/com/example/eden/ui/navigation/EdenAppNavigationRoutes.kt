835340d0930369e49b27e786136a26706ae2dba6
t3chkid <54663474+t3chkid@users.noreply.github.com>
2021-11-24 16:18:18 +0530

    refactor: add reportScreenRoute to EdenAppNavigationRoutes

--- app/src/main/java/com/example/eden/ui/navigation/EdenAppNavigationRoutes.kt
+++ app/src/main/java/com/example/eden/ui/navigation/EdenAppNavigationRoutes.kt
@@ -4,5 +4,5 @@ object EdenAppNavigationRoutes {
     const val onBoardingRoute = "com.example.eden.ui.navigation.EdenAppNavigationRoutes.onBoardingRoute"
     const val homeScreenRoute = "com.example.eden.ui.navigation.EdenAppNavigationRoutes.homeScreenRoute"
     const val notificationsScreenRoute = "com.example.eden.ui.navigation.EdenAppNavigationRoutes.notificationsScreen"
-
+    const val reportScreenRoute = "com.example.eden.ui.navigation.EdenAppNavigationRoutes.reportScreenRoute"
 }
